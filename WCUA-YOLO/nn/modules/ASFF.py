d59b982442d1fb935bd91213eea6bb99b87cc6d5
MachineEyes001 <luodong1867@163.com>
2024-11-26 14:36:02 +0800

    first commit

---
+++ WCUA-YOLO/nn/modules/ASFF.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+from ultralytics.utils.tal import dist2bbox, make_anchors
+import math
+import torch.nn.functional as F
+
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, padding, groups, dilation, activation)."""
+    default_act = nn.SiLU()  # default activation
+
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        """Initialize Conv layer with given arguments including activation."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn = nn.BatchNorm2d(c2)
+        self.act = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        """Apply convolution, batch normalization and activation to input tensor."""
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        """Perform transposed convolution of 2D data."""
+        return self.act(self.conv(x))
+
+
+class DFL(nn.Module):
+    """
+    Integral module of Distribution Focal Loss (DFL).
+    Proposed in Generalized Focal Loss https://ieeexplore.ieee.org/document/9792391
+    """
+
+    def __init__(self, c1=16):
+        """Initialize a convolutional layer with a given number of input channels."""
+        super().__init__()
+        self.conv = nn.Conv2d(c1, 1, 1, bias=False).requires_grad_(False)
+        x = torch.arange(c1, dtype=torch.float)
+        self.conv.weight.data[:] = nn.Parameter(x.view(1, c1, 1, 1))
+        self.c1 = c1
+
+    def forward(self, x):
+        """Applies a transformer layer on input tensor 'x' and returns a tensor."""
+        b, c, a = x.shape  # batch, channels, anchors
+        return self.conv(x.view(b, 4, self.c1, a).transpose(2, 1).softmax(1)).view(b, 4, a)
+        # return self.conv(x.view(b, self.c1, 4, a).softmax(1)).view(b, 4, a)
+
+
+
+
---
+++ WCUA-YOLO/nn/modules/CUConv.py
@@ -0,0 +1,141 @@
+import torch
+import torch.nn as nn
+
+
+class ConvBNPReLU(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1):
+        """
+        args:
+            nIn: number of input channels
+            nOut: number of output channels
+            kSize: kernel size
+            stride: stride rate for down-sampling. Default is 1
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2)
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), bias=False)
+        self.bn = nn.BatchNorm2d(nOut, eps=1e-03)
+        self.act = nn.PReLU(nOut)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        output = self.bn(output)
+        output = self.act(output)
+        return output
+
+
+class BNPReLU(nn.Module):
+    def __init__(self, nOut):
+        """
+        args:
+           nOut: channels of output feature maps
+        """
+        super().__init__()
+        self.bn = nn.BatchNorm2d(nOut, eps=1e-03)
+        self.act = nn.PReLU(nOut)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: normalized and thresholded feature map
+        """
+        output = self.bn(input)
+        output = self.act(output)
+        return output
+
+
+
+
+
+class Conv(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1):
+        """
+        args:
+            nIn: number of input channels
+            nOut: number of output channels
+            kSize: kernel size
+            stride: optional stride rate for down-sampling
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2)
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), bias=False)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        return output
+
+
+class ChannelWiseConv(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1):
+        """
+        Args:
+            nIn: number of input channels
+            nOut: number of output channels, default (nIn == nOut)
+            kSize: kernel size
+            stride: optional stride rate for down-sampling
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2)
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), groups=nIn,
+                              bias=False)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        return output
+
+
+class DilatedConv(nn.Module):
+    def __init__(self, nIn, nOut, kSize, stride=1, d=1):
+        """
+        args:
+           nIn: number of input channels
+           nOut: number of output channels
+           kSize: kernel size
+           stride: optional stride rate for down-sampling
+           d: dilation rate
+        """
+        super().__init__()
+        if isinstance(kSize, tuple):
+            kSize = kSize[0]
+        padding = int((kSize - 1) / 2) * d
+        self.conv = nn.Conv2d(nIn, nOut, (kSize, kSize), stride=stride, padding=(padding, padding), bias=False,
+                              dilation=d)
+
+    def forward(self, input):
+        """
+        args:
+           input: input feature map
+           return: transformed feature map
+        """
+        output = self.conv(input)
+        return output
+
+
+
+
+
+
+
+
